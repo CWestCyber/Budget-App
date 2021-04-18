744b950d2d4e226c1d23e5440f0cc8f025f3e50e
CWestCyber <82707823+CWestCyber@users.noreply.github.com>
2021-04-18 01:03:51 +0100

    Add files via upload

---
+++ Budget App.py
@@ -0,0 +1,17 @@
+Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /Users/cweststudios/Documents/class.py ===============
+Budget_1.name
+Budget_2.name
+Budget_3.name
+>>> 
+================ RESTART: /Users/cweststudios/Documents/class.py ===============
+1000
+800
+800
+500
+300
+300
+>>> 
